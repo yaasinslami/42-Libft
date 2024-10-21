63cebe947300a3fed7ae223370b1d7dea9a4ba2e
yaasinslami <yassinx4002@gmail.com>
2024-10-21 14:17:14 +0100

    add 42_libft

---
+++ ft_strlcat.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yslami <yslami@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 17:50:20 by yslami            #+#    #+#             */
+/*   Updated: 2024/10/21 15:16:52 by yslami           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+unsigned int	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	;
+	while (src[j])
+}
