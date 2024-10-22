9c41c107e91a988418d0b3b7e7da480b49640c20
yaasinslami <yassinx4002@gmail.com>
2024-10-22 17:26:32 +0100

    add 42_libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: yslami <yslami@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 18:06:10 by yslami            #+#    #+#             */
-/*   Updated: 2024/10/22 12:19:58 by yslami           ###   ########.fr       */
+/*   Updated: 2024/10/22 17:26:27 by yslami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@
 # include <string.h>
 # include <stdio.h>
 
-int		ft_strlen(const char *str);
-char	*ft_strcat(char *src, char *dest);
-char	*ft_stnrstr(const char *str, const char *to_find);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	*ft_memset(void *s, int c, size_t n);
-void	ft_bzero(void *s, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
+int				ft_strlen(const char *str);
+char			*ft_strcat(char *src, char *dest);
+unsigned int	ft_strlcat(char *dest, const char *src, size_t size);
+char			*ft_stnrstr(const char *str, const char *to_find);
+int				ft_strncmp(const char *s1, const char *s2, size_t n);
+void			*ft_memset(void *s, int c, size_t n);
+void			ft_bzero(void *s, size_t n);
+void			*ft_memcpy(void *dest, const void *src, size_t n);
+void			*ft_memmove(void *dest, const void *src, size_t n);
 
 #endif
